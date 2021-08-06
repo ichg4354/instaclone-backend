913f38e8c13bcab63b842de3a8bd1a701ee64bb1
user <60804817+ichg4354@users.noreply.github.com>
2021-08-06 21:05:30 +0900

    add createaccount username and firstname check on resolver

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,20 @@
+import { client } from "../client";
+
 export default {
-  Mutation: {},
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstname, lastname, username, email, password }
+    ) => {
+      const searchedUser = await client.user.findFirst({
+        where: {
+          OR: [{ username: username }, { firstname: firstname }],
+        },
+      });
+      console.log(searchedUser);
+      // check if firstname and username is taken
+      // hash password
+      // return selected profile
+    },
+  },
 };
