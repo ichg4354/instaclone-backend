3bb41b4a348204f26fc704e50f4142b92a348170
user <60804817+ichg4354@users.noreply.github.com>
2021-08-08 21:36:02 +0900

    finish edit profile resolver

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,35 @@
+import { client } from "../../client";
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
-    editProfile: (_, {}) => console.log("HELLO"),
+    editProfile: async (
+      _,
+      { firstname, lastname, username, email, password: newPassword }
+    ) => {
+      let uglyPassword = undefined;
+
+      // check if password is edited to hash
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+
+      const updatedUser = await client.user.update({
+        where: { id: 2 },
+        data: {
+          firstname,
+          lastname,
+          username,
+          email,
+          password: uglyPassword,
+        },
+      });
+
+      if (updatedUser.id) {
+        return { ok: true };
+      } else {
+        return { ok: false, error: "Could not update Profile" };
+      }
+    },
   },
 };
